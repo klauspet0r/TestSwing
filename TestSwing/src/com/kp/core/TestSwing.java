0d9ce71bebe67361599a93353c6b6df49858a768
michal.zajac <michal.zajac@ml-pa.com>
2017-07-10 12:28:53 +0200

    idk

--- TestSwing/src/com/kp/core/TestSwing.java
+++ TestSwing/src/com/kp/core/TestSwing.java
@@ -1,9 +1,13 @@
 package com.kp.core;
 
+import java.awt.Button;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Font;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.border.Border;
@@ -13,30 +17,43 @@ public class TestSwing {
 
 	public static void main(String[] args) {
 		
-		//Create and set up the window.
-        JFrame frame = new JFrame("HelloWorldSwing");
+		
+        JFrame frame = new JFrame("HelloWorld");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
- 
-        //Add the ubiquitous "Hello World" label.
-        JLabel label = new JLabel("Hello World");
-        label.setFont(new Font("Serif", Font.PLAIN, 99));
         
-        label.setHorizontalAlignment(0);
-        frame.getContentPane().add(label);
+        	
         
-        
-        
-//        Border raisedbevel = BorderFactory.createRaisedBevelBorder();
-        Border raisedbevel = BorderFactory.createLineBorder(Color.black);
-        label.setBorder(raisedbevel);
-        
-        //Display the window.
-        frame.pack();
-        frame.setLocation(800, 500);
-        frame.setSize(label.getSize().width+80, label.getSize().height);
-        frame.setVisible(true);
-        frame.getContentPane().setBackground(Color.RED);
-        
+//        frame.setUndecorated(true);
+// 
+//        //Add the ubiquitous "Hello World" label.
+//        JLabel label = new JLabel("Hello World");
+//        label.setFont(new Font("Serif", Font.PLAIN, 99));
+//        
+//        label.setHorizontalAlignment(0);
+//        frame.getContentPane().add(label);
+//        
+//        Container container = new Container();
+//        
+//        container.add(Box.createRigidArea(new Dimension(0, 10)));
+//        container.add(label);
+//        
+//        frame.add(container);
+//       
+//
+//        Border raisedbevel = BorderFactory.createLineBorder(Color.red);
+//        label.setBorder(raisedbevel);
+//        
+//        
+//        
+//        //Display the window.
+//        frame.pack();
+//        frame.setLocation(800, 500);
+//        frame.getContentPane().setSize(label.getSize().width+80, label.getSize().height+80);
+//        frame.setSize(label.getSize().width+180, label.getSize().height+180);
+//        
+//        frame.setVisible(true);
+//        frame.getContentPane().setBackground(Color.white);
+//        
 
 	}
 
